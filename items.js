c042a89696fecc2c9af01b522a621eacbff01970
tehzarathustra <tehzarathustra@gmail.com>
2024-08-24 18:31:41 +0200

    init commit

---
+++ items.js
@@ -0,0 +1,79 @@
+const AGHANIM = {
+  str: 10,
+  int: 10,
+  agi: 10,
+  hp: 175,
+  mp: 175
+};
+
+const WITCH_BLADE = {
+  int: 12,
+  mp_regen: 1.5,
+  armor: 6,
+  attack_speed: 40
+};
+
+const WRAITH_BAND_MIN = {
+  str: 2,
+  agi: 5,
+  int: 2,
+  armor: 1.75,
+  attack_speed: 5
+};
+
+const WRAITH_BAND_MAX = {
+  str: 4,
+  agi: 10,
+  int: 4,
+  armor: 3.5,
+  attack_speed: 10
+};
+
+const BRACER_MIN = {
+  str: 5,
+  agi: 2,
+  int: 2,
+  hp_regen: 0.75,
+  attack: 3
+};
+
+const BRACER_MAX = {
+  str: 10,
+  agi: 4,
+  int: 4,
+  hp_regen: 1.5,
+  attack: 6
+};
+
+const NULL_TALISMAN_MIN = {
+  str: 2,
+  agi: 2,
+  int: 5,
+  mp: 3,
+  mp_regen: 1
+};
+
+const NULL_TALISMAN_MAX = {
+  str: 4,
+  agi: 4,
+  int: 10,
+  mp: 6,
+  mp_regen: 2
+};
+
+const POWER_TREADS = {
+  str: 10,
+  attack_speed: 25
+};
+
+export {
+  AGHANIM,
+  WITCH_BLADE,
+  WRAITH_BAND_MIN,
+  WRAITH_BAND_MAX,
+  BRACER_MIN,
+  BRACER_MAX,
+  NULL_TALISMAN_MIN,
+  NULL_TALISMAN_MAX,
+  POWER_TREADS
+};
